1a5cd0cb0c92d09937e4f8315a00c1cd7dcb9e03
= <yemunRussom10@gmail.com>
2022-11-08 17:03:06 +0000

    Extension1 done

--- src/app.js
+++ src/app.js
@@ -11,6 +11,33 @@ function App() {
   // Use initialEmails for state
   console.log(emails)
 
+
+  const toggleStar = (starUpdate) => {
+    const starList = emails.map((email) => {
+      if(email === starUpdate){
+        return{...email, starred: !email.starred}
+      }
+
+      return email;
+    }) 
+
+    setEmails(starList)
+    console.log(emails)
+  }
+
+  const toggleRead = (readUpdate) => {
+    const readList = emails.map((email) => {
+      if(email === readUpdate){
+        return{...email, read: !email.read}
+      }
+
+      return email;
+    }) 
+
+    setEmails(readList)
+    console.log(emails)
+  }
+
   return (
     <div className="app">
       <Header />
@@ -44,19 +71,19 @@ function App() {
       </nav>
       <main className="emails">{
        emails.map(function(email) {
-        return <li className="email" key={email.id}>
-        <div className="select" key={email.id}>
+        return <li className = {email.read ? 'email read': 'email unread'}
+        key={email.id}>
+        <div className="select">
         <input
           className="select-checkbox"
           type="checkbox"
-          key={email.id}/>
+          onClick={ () => toggleRead(email)}/>
         </div>
-        <div className="star" key={email.id}>
+        <div className="star">
         <input
           className="star-checkbox"
           type="checkbox"
-          key={email.id}
-        />
+          onClick={() => toggleStar(email)}/>
         </div>
         <div className="sender">{email.sender}</div>
         <div className="title">{email.title}</div>
